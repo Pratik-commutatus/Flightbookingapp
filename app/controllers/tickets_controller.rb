a53f86386ccdc4f5601afff5ced629dc52437a41
PRATIK DHARAMDASANI <pratik@commutatus.com>
2020-03-23 23:02:53 +0530

    rollback

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,7 +7,7 @@ class TicketsController < ApplicationController
   # GET /tickets
   # GET /tickets.json
   def index
-    @tickets = Ticket.all.where(user_id: current_user.id)
+    @tickets = Ticket.all
   end
 
   # GET /tickets/1
