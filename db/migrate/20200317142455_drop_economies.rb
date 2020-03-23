729d92754923018eed12d4c79bee362ee60931cd
PRATIK DHARAMDASANI <pratik@commutatus.com>
2020-03-23 20:59:11 +0530

    fix migation attempt 1

--- db/migrate/20200317142455_drop_economies.rb
+++
@@ -1,5 +0,0 @@
-class DropEconomies < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :economies
-  end
-end
--- db/migrate/20200319131357_drop_table_tickets.rb
+++
@@ -1,5 +0,0 @@
-class DropTableTickets < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :tickets
-  end
-end
